aec80d473452e769e0a5221b9ef7c839fc70eb53
Jared Harley <jared@jaredharley.com>
2015-03-16 22:48:28 -0600

    Added multiple data providers
    Added interface structure for mulitple data providers, and API key
    loading from .key files.

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 // The weatherData struct that handles the returned weather data
@@ -15,8 +19,27 @@ type weatherData struct {
 	} `json:"main"`
 }
 
+// Weather provider interface
+type weatherProvider interface {
+	temperature(city string) (float64, error)
+}
+
+type openWeatherMap struct{}
+type weatherUnderground struct {}
+type multiWeatherProvider []weatherProvider
+
+var wuKey string
+var mw multiWeatherProvider
+
 // Main entry point for the program.
 func main() {
+	getAPIKeys()
+
+	mw = multiWeatherProvider {
+		openWeatherMap{},
+		weatherUnderground{},
+	}
+
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/weather/", weather)
 	http.ListenAndServe(":8000", nil)
@@ -27,38 +50,107 @@ func hello(writer http.ResponseWriter, req *http.Request) {
 	writer.Write([]byte("Hello!"))
 }
 
+func getAPIKeys() {
+	// Weather Underground
+	key, err := ioutil.ReadFile("weatherunderground.key")
+	if err != nil {
+		fmt.Printf("Unable to read weatherunderground keyfile.\n")	
+	} else {
+		fmt.Printf("Weather Underground API key loaded: %s\n", key)
+	}
+	wuKey = string(key)
+}
+
 // weather is the http handler function for utilizing the weather API. It processes
 // the URL, calls the query function, and writes the output of that function to the
 // response stream. If an error object is returned by the query function, an Http 500
 // error is written to the response stream.
 func weather(writer http.ResponseWriter, req *http.Request) {
+	begin := time.Now()
 	city := strings.SplitN(req.URL.Path, "/", 3)[2]
-	data, err := query(city)
+
+	temp, err := mw.temperature(city)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(writer).Encode(data)
+	json.NewEncoder(writer).Encode(map[string]interface{}{
+		"city": city,
+		"temp": temp,
+		"took": time.Since(begin).String(),
+	})
+
 }
 
 // query takes the name of a city as a string and queries the OpenWeatherMap API
 // for weather data. This function either returns a weatherData struct of the 
 // returned data, or an error object.
-func query(city string) (weatherData, error) {
+func (w openWeatherMap) temperature(city string) (float64, error) {
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city)
 	if err != nil {
-		return weatherData{}, err
+		return 0, err
 	}
 
 	defer resp.Body.Close()
 
-	var d weatherData
-
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
+	var d struct {
+		Main struct {
+			Kelvin float64 `json:"temp"`
+		} `json:"main"`
 	}
 
-	return d, nil
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	fmt.Printf("OpenWeatherMap responded with %.2fK for %s\n", d.Main.Kelvin, city)
+
+	return d.Main.Kelvin, nil
+}
+
+func (w weatherUnderground) temperature(city string) (float64, error) {
+	if wuKey == "" {
+		return 0, errors.New("Weather Underground API key must be set")
+	}
+
+	resp, err := http.Get("http://api.wunderground.com/api/" + wuKey + "/conditions/q/" + city + ".json")
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Observation struct {
+			Celcius float64 `json:"temp_c"`
+		} `json:"current_observation"`
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	kelvin := d.Observation.Celcius + 273.15
+	fmt.Printf("Weather Underground responded with %.2fK for %s\n", kelvin, city)
+
+	return kelvin, nil
+}
+
+func (w multiWeatherProvider) temperature(city string) (float64, error) {
+	sum := 0.0
+
+	for _, provider := range w {
+		k, err := provider.temperature(city)
+		f := (k * 1.8) - 459.67
+		fmt.Printf("%.2fK converts to %.2fF\n", k, f)
+		if err != nil {
+			return 0, err
+		}
+
+		sum += f
+	}
+
+	return sum / float64(len(w)), nil
 }
